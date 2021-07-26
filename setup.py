07bf50d86ac6ff413ba1a9e1c5fc56077c8ccfb8
Jamie Titus <ctitus@stanford.edu>
2021-07-26 15:44:01 -0400

    package renamed to sst_common_sim for consistency

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     author="Charles Titus",
     author_email="charles.titus@nist.gov",
     install_requires=["bluesky", "ophyd"],
-    name="sst_sim_shims",
+    name="sst_common_sim",
+    entry_points = {"sst_common": ["sst_sim = sst_common_sim.api"]},
     use_scm_version=True,
     packages=find_packages()
 )
