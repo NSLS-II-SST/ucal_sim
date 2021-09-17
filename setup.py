6090acc222925e295275b8876efbea8888679f37
Charles Titus <ctitus@bnl.gov>
2021-09-17 10:27:36 -0400

    updates to setup requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     author="Charles Titus",
     author_email="charles.titus@nist.gov",
-    install_requires=["bluesky", "ophyd"],
+    install_requires=["bluesky", "ophyd", "numpy", "scipy"],
     name="sst_common_sim",
     entry_points = {"sst_common": ["sst_sim = sst_common_sim.api"]},
     use_scm_version=True,
