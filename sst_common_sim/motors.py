c355121fed1c9304beb06be77a69680a44fb5a5b
Charles Titus <ctitus@bnl.gov>
2022-01-17 21:25:02 -0500

    manipulators updated, removed silly namespace from api

--- sst_common_sim/motors.py
+++ sst_common_sim/motors.py
@@ -1,13 +1,17 @@
-from sst_base.manipulator import Manipulator4AxBase, Manipulator4AxTest
+from sst_base.manipulator import Manipulator4AxBase, Manipulator1AxBase
 from ophyd import Component as Cpt
 from ophyd.positioner import SoftPositioner
 
 
-class Manipulator(Manipulator4AxTest):
+class Manipulator(Manipulator4AxBase):
     x = Cpt(SoftPositioner, name='Sample X', init_pos=0.0)
     y = Cpt(SoftPositioner, name='Sample Y', init_pos=0.0)
     z = Cpt(SoftPositioner, name='Sample Z', init_pos=0.0)
     r = Cpt(SoftPositioner, name='Sample R', init_pos=0.0)
+
+
+class MultiMesh(Manipulator1AxBase):
+    x = Cpt(SoftPositioner, name='Multimesh', init_pos=0.0)
 
 
 tesz = SoftPositioner(name='TES Z', init_pos=0.0)
